ff666970df30d906746bc03e4247c54d1adeef97
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-20 23:04:26 +0300

    Wrapped Provider (app)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App.tsx'
+import { store } from './store'
 
 import './styles/index.scss'
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
