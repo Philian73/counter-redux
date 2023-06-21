6f15880a2dcc741b40c8baa8e96c45d66960456d
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-21 23:48:58 +0300

    Refactor

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,5 @@
 export type CounterType = {
   id: string
-}
-
-export type SettingsCounterType = {
-  isReady: boolean
-  currentValue: number
   minValue: number
   maxValue: number
-}
-
-export type SettingsCountersType = {
-  [key: string]: SettingsCounterType
 }
