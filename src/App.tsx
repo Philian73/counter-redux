0dddfaf3773abdf1e0a5fc7b2d8766e813ca1f01
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-21 23:54:14 +0300

    Render Counters.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useSelector } from 'react-redux'
-
-import { Counter } from './components/Counter/Counter.tsx'
-import { AppRootStateType } from './store'
-import { CounterType } from './types'
+import { Counters } from './components/Counters/Counters.tsx'
 
 const App = () => {
   console.log('App rendering')
-  const counters = useSelector<AppRootStateType, CounterType[]>(state => state.counters)
 
   return (
     <div className={'app'}>
-      {counters.map(counter => (
-        <Counter key={counter.id} counterId={counter.id} />
-      ))}
+      <Counters />
     </div>
   )
 }
