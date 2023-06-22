403e89c53a6cd498511451d2e66f4197b6ed6b78
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-23 00:19:54 +0300

    useEffect (import getCounters, addCounter thunks)

--- src/components/Counters/Counters.tsx
+++ src/components/Counters/Counters.tsx
@@ -1,20 +1,35 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { AppRootStateType } from '../../store'
+import { AppDispatchType, AppRootStateType } from '../../store'
+import { addCounter, getCounters } from '../../store/reducers/countersReducer.ts'
 import { CounterType } from '../../types'
+import { Button } from '../Button/Button.tsx'
 
 import { Counter } from './Counter/Counter.tsx'
 
 type PropsType = {}
 export const Counters: FC<PropsType> = ({}) => {
   console.log('CounterSSSS rendering')
+
   const counters = useSelector<AppRootStateType, CounterType[]>(state => state.counters)
+  const dispatch = useDispatch<AppDispatchType>()
+
+  useEffect(() => {
+    dispatch(getCounters())
+  }, [])
 
   const countersMap = counters.map(counter => {
     return <Counter key={counter.id} counter={counter} />
   })
 
-  return <>{countersMap}</>
+  return (
+    <>
+      <div>{countersMap}</div>
+      <div>
+        <Button onClick={() => dispatch(addCounter())}>Добавить счётчик</Button>
+      </div>
+    </>
+  )
 }
