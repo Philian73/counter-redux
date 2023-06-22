6885567661aecb8096d5a8b6aea7ba41abe0e6ae
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-22 18:03:55 +0300

    Logic

--- src/components/Counters/Counters.tsx
+++ src/components/Counters/Counters.tsx
@@ -1,6 +1,20 @@
 import { FC } from 'react'
 
+import { useSelector } from 'react-redux'
+
+import { AppRootStateType } from '../../store'
+import { CounterType } from '../../types'
+
+import { Counter } from './Counter/Counter.tsx'
+
 type PropsType = {}
 export const Counters: FC<PropsType> = ({}) => {
-  return <div></div>
+  console.log('CounterSSSS rendering')
+  const counters = useSelector<AppRootStateType, CounterType[]>(state => state.counters)
+
+  const countersMap = counters.map(counter => {
+    return <Counter key={counter.id} counter={counter} />
+  })
+
+  return <>{countersMap}</>
 }
