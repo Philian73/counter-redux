40caf9f8cda23490f0ff6f65175dfb46d98b6119
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-23 23:33:59 +0300

    import custom hook: useCounterLogic

--- src/components/Counters/Counter/Counter.tsx
+++ src/components/Counters/Counter/Counter.tsx
@@ -1,67 +1,28 @@
-import { ChangeEvent, FC, memo, useState } from 'react'
+import { FC, memo } from 'react'
 
-import { useDispatch } from 'react-redux'
-
-import { AppDispatchType } from '../../../store'
-import { updateSettings } from '../../../store/reducers/countersReducer.ts'
-import { CounterInputModelType, CounterType } from '../../../types'
+import { CounterType } from '../../../types'
 import { Button } from '../../Button/Button.tsx'
 
 import cls from './Counter.module.scss'
+import { useCounterLogic } from './hooks/useCounterLogic.ts'
 
 type PropsType = {
   counter: CounterType
 }
+
 export const Counter: FC<PropsType> = memo(({ counter }) => {
-  console.log('Counter rendering')
-  const [status, setStatus] = useState('')
-  const [values, setValues] = useState<CounterInputModelType>({
-    minValue: counter.minValue,
-    maxValue: counter.maxValue,
-    currentValue: counter.minValue,
-  })
-
-  const { minValue, maxValue, currentValue } = values
-
-  const dispatch = useDispatch<AppDispatchType>()
-
-  const increment = () => {
-    if (currentValue < maxValue) {
-      setValues(prev => ({ ...prev, currentValue: prev.currentValue + 1 }))
-    }
-  }
-  const reset = () => {
-    setValues(prev => ({ ...prev, currentValue: minValue }))
-  }
-  const changeModeToSettings = () => {
-    !status && setStatus('Настройка счётчика')
-  }
-  const saveChanges = () => {
-    if (minValue < maxValue) {
-      dispatch(updateSettings(counter.id, minValue, maxValue))
-      setStatus('')
-      reset()
-    }
-  }
-
-  const onChangeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
-    const valueToNumber = Number(e.currentTarget.value)
-
-    if (valueToNumber > -1 && valueToNumber < maxValue) {
-      setValues(prev => ({ ...prev, minValue: valueToNumber }))
-    }
-  }
-  const onChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
-    const valueToNumber = Number(e.currentTarget.value)
-
-    if (valueToNumber > minValue) {
-      setValues(prev => ({ ...prev, maxValue: valueToNumber }))
-    }
-  }
-
-  if (counter.minValue === 0 && counter.maxValue === 0) {
-    changeModeToSettings()
-  }
+  const {
+    currentValue,
+    minValue,
+    onChangeMinValue,
+    maxValue,
+    increment,
+    changeModeToSettings,
+    saveChanges,
+    reset,
+    onChangeMaxValue,
+    status,
+  } = useCounterLogic(counter)
 
   return (
     <div className={cls.counter}>
