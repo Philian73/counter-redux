e6349e4b6551e2f1eac857182f4d0b21a121940c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-24 23:50:20 +0300

    import styles

--- src/components/Counters/Counters.tsx
+++ src/components/Counters/Counters.tsx
@@ -8,6 +8,7 @@ import { CounterType } from '../../types'
 import { Button } from '../Button/Button.tsx'
 
 import { Counter } from './Counter/Counter.tsx'
+import cls from './Counters.module.scss'
 
 export const Counters = () => {
   const counters = useSelector<AppRootStateType, CounterType[]>(state => state.counters)
@@ -27,11 +28,11 @@ export const Counters = () => {
   })
 
   return (
-    <>
-      <div>{countersMap}</div>
+    <div>
       <div>
         <Button onClick={addCounterCallback}>Добавить счётчик</Button>
       </div>
-    </>
+      <div className={cls.counters}>{countersMap}</div>
+    </div>
   )
 }
